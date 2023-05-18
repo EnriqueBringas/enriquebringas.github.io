f93d870b45fe58311891773078f276971cd328fe
David Bringas <enriquebringasaviles@hotmail.com>
2023-05-18 04:21:34 -0500

    feat: add sub application CodeGenerator

---
+++ CodeGenerator/wwwroot/js/sql/sql.draggable.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+    $("body").on("mousedown", ".sql-table-group", function (e) {
+        var obj = $(this);
+        var table = obj.find(".sql-table thead");
+        var dr = obj.addClass("drag");
+        var height = dr.outerHeight();
+        var width = dr.outerWidth();
+        var ypos = dr.offset().top + height - e.pageY;
+        var xpos = dr.offset().left + width - e.pageX;
+
+        $(document.body).on("mousemove", function (e) {
+            var itop = e.pageY + ypos - height;
+            var ileft = e.pageX + xpos - width;
+
+            if (dr.hasClass("drag"))
+                dr.offset({ top: itop, left: ileft });
+        }).on("mouseup", function (e) {
+            dr.removeClass("drag");
+            updPositionTable(table, obj.css("top"), obj.css("left"));
+        });
+    });
+});
+
+function updPositionTable(table, top, left) {
+    var lid = table.attr("data-id");
+    var ltable = $.map(ldatajson, function (val, key) {
+        if (val.id == lid)
+            return val;
+    });
+
+    if (ltable.length > 0) {
+        ltable[0].style = "position: relative; top: " + top + "; left: " + left + ";";
+
+        setLocalData(keydatabase, ldatajson);
+    }
+}
---
+++ CodeGenerator/wwwroot/js/sql/sql.settings.js
@@ -0,0 +1,40 @@
+﻿var lsettingsjson = {};
+var ldatajson = [];
+
+var keysettings = "ideaslokas-modelsql-settings";
+var keydatabase = "ideaslokas-modelsql-database";
+
+$(document).ready(function () {
+    var ltempsettings = getLocalData(keysettings);
+    var ltempdatabase = getLocalData(keydatabase);
+
+    if (ltempsettings == null)
+        $.getJSON("json/settings.json", function (json) {
+            lsettingsjson = json;
+            setLocalData(keysettings, json);
+            console.log("settings by server");
+        });
+    else {
+        lsettingsjson = ltempsettings;
+        console.log("settings by local");
+    }
+
+    if (ltempdatabase != null) {
+        ldatajson = ltempdatabase;
+        drawTables();
+        console.log("database by local");
+    }
+});
+
+function setLocalData(key, value) {
+    window.localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getLocalData(key) {
+    var linfo = window.localStorage.getItem(key);
+
+    if (typeof linfo === "undefined")
+        return null;
+    else
+        return JSON.parse(linfo);
+}
